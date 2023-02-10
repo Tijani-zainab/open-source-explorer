67fcc935c0f1115c87f867b8439986d8eb7d464d
Zainab Tijani <jani.tijj@gmail.com>
2023-02-10 20:45:58 +0100

    new PR for landing (#10)
    * completes landing page
    
    * fix changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React from "react";
 import "./App.scss";
 import Particles from "react-tsparticles";
-// import { Landing } from "./Pages";
+import { Landing } from "./Pages";
 import { loadStarsPreset } from "tsparticles-preset-stars";
 import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
-import FilterProject from "./Pages/filterProject";
 
 const App = () => {
   const options = {
     preset: "stars",
+    particles: {
+      size: {
+        value: 1, // change the value property to reduce the size of the stars
+        random: false,
+        anim: {
+          enable: false,
+        },
+      },
+      number: {
+        value: 300, // increase the value to add more stars
+      }, 
+    },
+
     Animation: {
       enable: true,
       speed: 30,
       minimumValue: 0.1,
       sync: false,
+    },
+
+    background: {
+      color: {
+        value: "#231e43",
+      },
     },
   };
 
@@ -26,8 +44,7 @@ const App = () => {
       <div className="components-containter">
         <ErrorBoundary>
           {/* <Routing /> */}
-          <FilterProject />
-          {/* <Landing /> */}
+          <Landing />
         </ErrorBoundary>
       </div>
 
